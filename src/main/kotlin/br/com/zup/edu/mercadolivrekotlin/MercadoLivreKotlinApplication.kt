1c6798fcf7a341766864c647e51f619325a6aeaa
Yuri Matheus <yuri.oliveira@zup.com.br>
2021-11-26 09:56:50 -0300

    first commit

---
+++ src/main/kotlin/br/com/zup/edu/mercadolivrekotlin/MercadoLivreKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.zup.edu.mercadolivrekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MercadoLivreKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MercadoLivreKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/zup/edu/mercadolivrekotlin/MercadoLivreKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.zup.edu.mercadolivrekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MercadoLivreKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
